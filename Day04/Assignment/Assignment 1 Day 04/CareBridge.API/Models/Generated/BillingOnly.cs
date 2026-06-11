e17da4aaf97c0f2f930442e9f38c57e513bc4370
Sri28-charan <sriinampudi28@gmail.com>
2026-06-11 05:37:09 -0700

    Day 04: Added Labs and Assignments - Patient Search API & Department Encounter Dashboard

---
+++ Day04/Assignment/Assignment 1 Day 04/CareBridge.API/Models/Generated/BillingOnly.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CareBridge.EFCoreDemo.Models.Generated;
+
+public partial class BillingOnly
+{
+    public int ClaimId { get; set; }
+
+    public int EncounterId { get; set; }
+
+    public string Status { get; set; } = null!;
+
+    public decimal BilledAmount { get; set; }
+
+    public decimal? ReimbursedAmt { get; set; }
+
+    public decimal? OutstandingAmount { get; set; }
+}
