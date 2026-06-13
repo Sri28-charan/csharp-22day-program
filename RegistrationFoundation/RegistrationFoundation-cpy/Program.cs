56112de19e7f0a257460e3fb7fb2af2e2bfda93d
Sri28-charan <sriinampudi28@gmail.com>
2026-06-13 03:35:57 -0700

    Delete RegistrationFoundation directory

--- RegistrationFoundation/RegistrationFoundation-cpy/Program.cs
+++
@@ -1,9 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-app.UseDefaultFiles();   // index.html loads at /
-app.UseStaticFiles();    // Serves .html .css .js from wwwroot
-
-app.Run();
-
-
